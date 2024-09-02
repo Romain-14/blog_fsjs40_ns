87fe1a5e7d9ad6828770121502f16d73919622ee
Romain-14 <romain14f@msn.com>
2024-09-02 12:25:58 +0200

    initial commit template config ejs DB archi

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+
+import router from "./router/index.routes.js";
+// pour tester la connexion à la BDD avec la méthode getConnection
+// à supprimer après le test
+// import pool from "./config/db.js";
+
+const app = express();
+const PORT = process.env.LOCAL_PORT;
+
+// configuration de moteur de template
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "src/view"));
+// configuration de dossier statique
+app.use("/css", express.static(path.join(process.cwd(), "public/css")));
+app.use("/img", express.static(path.join(process.cwd(), "public/img")));
+
+// middleware pour le router
+app.use(router);
+
+
+app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+// Test de la connexion à la BDD
+// à commenter quand le test est terminé
+// pool.getConnection()
+//     .then((connection) => {
+//         console.log(`Connected to database ${connection.config.database}`);
+//         connection.release();
+//     })
+//     .catch(error => console.error(`Error connecting to database: ${error.message}`));
+
+
+export default pool;
